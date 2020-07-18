19cc0e84840eb3558b51f81761d054968ad66019
Muhammad Mubashir <66302833+MubashirAslam78@users.noreply.github.com>
2020-07-18 17:23:28 +0500

    Initial Commit

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+    secondary: {
+      main: "#000000",
+    },
+  },
+  typography: {
+    fontFamily: "'Sahitya', serif",
+  },
+});
---
+++ src/utils/social.js
@@ -0,0 +1,3 @@
+export const facebookURL = "https://web.facebook.com/profile.php?id=100011147382310";
+export const githubURL = "https://github.com/MubashirAslam78";
+
